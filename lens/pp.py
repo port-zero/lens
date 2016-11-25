bdd1bb304b75e981a4447b2474a98946f6d3ce60
hellerve <veit.heller@student.htw-berlin.de>
2016-11-25 19:15:11 +0100

    if no lexer is specified, do not highlight

--- lens/pp.py
+++ lens/pp.py
@@ -2,4 +2,9 @@ import pygments
 from pygments.formatters import TerminalFormatter
 
 def prettyprint(module, output):
-    print(pygments.highlight(output, module.lexer(), TerminalFormatter()))
+    if module.lexer:
+        output = pygments.highlight(output,
+                                    module.lexer(),
+                                    TerminalFormatter())
+
+    print(output)
