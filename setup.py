06b52dd8ac3dde2f7f162a567f8e5e961e7209b1
hellerve <veit@veitheller.de>
2019-08-12 18:25:28 +0200

    vbump

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open('README.md') as f:
 setup(
     name='lens-cli',
     author='Veit Heller',
-    version='0.1.3',
+    version='0.1.4',
     license='MIT',
     url='https://github.com/port-zero/lens',
     description='Extensible data structure traversal in the command line',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    download_url = 'https://github.com/port-zero/lens/tarball/0.1.3',
+    download_url = 'https://github.com/port-zero/lens/tarball/0.1.4',
     packages=find_packages('.'),
     install_requires=[
         "pygments",
